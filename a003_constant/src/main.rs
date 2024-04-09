4782c826f2b94364ca31c3ec0ee55ee2311a7ea2
ZhenFengWu <1994583917@qq.com>
2024-04-09 14:48:56 +0800

    静态变量

--- a003_constant/src/main.rs
+++ a003_constant/src/main.rs
@@ -2,6 +2,9 @@
 const SECOND_HOURS:usize = 3_600;
 const SECOND_DAY:usize = 24 * SECOND_HOURS;
 
+static MY_STATIC:i32 = 50;
+static mut MY_MUT_STATIC:i32 = 50;
+
 fn main() {
     println!("SECOND_HOURS:{SECOND_HOURS}");
     println!("SECOND_DAY:{SECOND_DAY}");
@@ -9,4 +12,16 @@ fn main() {
     const INFO:&str = "hello world";
     println!("INFO:{INFO}");
     
+    println!("MY_STATIC:{MY_STATIC}");
+
+    // mutable statics can be mutated by multiple threads: aliasing violations or data races will cause undefined behavior
+    // 可变静态可以被多个线程改变:违反别名或数据竞争将导致未定义的行为
+    //println!("MY_MUT_STATIC:{MY_MUT_STATIC}");
+    
+    // 使用unsafe，来操作可变静态
+    unsafe{
+        println!("MY_MUT_STATIC:{MY_MUT_STATIC}");
+        MY_MUT_STATIC = 100;
+        println!("MY_MUT_STATIC:{MY_MUT_STATIC}");
+    }
 }
