9a354521726ff5df2bb80bf4e2bbaedd2e6beef3
yushuishu <1994583917@qq.com>
2024-06-01 23:19:08 +0800

    Delete a004_data-types/target directory

--- a004_data-types/target/debug/deps/a004_data_types.d
+++
@@ -1,5 +0,0 @@
-F:\rust\workspace\base\rust-basic-learning\a004_data-types\target\debug\deps\a004_data_types.exe: src\main.rs
-
-F:\rust\workspace\base\rust-basic-learning\a004_data-types\target\debug\deps\a004_data_types.d: src\main.rs
-
-src\main.rs:
